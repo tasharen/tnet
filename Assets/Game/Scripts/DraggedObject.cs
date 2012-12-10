c17bc979144b1540fe7b10538a021cfafab059a0
Michael Lyashenko <arenmook@gmail.com>
2012-12-10 03:05:52 +0100

    - Improved the 3rd example by not using physics.

--- Assets/Game/Scripts/DraggedObject.cs
+++ Assets/Game/Scripts/DraggedObject.cs
@@ -11,39 +11,21 @@ using TNet;
 /// You can see it used on draggable cubes in Example 3.
 /// </summary>
 
-[RequireComponent(typeof(Rigidbody))]
 public class DraggedObject : TNBehaviour
 {
-	public int ownerID = -1;
-	public int playerID = -1;
-
 	Transform mTrans;
-	Rigidbody mRb;
-	Player mPlayer;
+	Player mOwner;
 	Vector3 mTarget;
 
 	void Awake ()
 	{
-		mRb = rigidbody;
 		mTrans = transform;
 		mTarget = mTrans.position;
 	}
 
-	/// <summary>
-	/// Apply force to the object, making it move toward the target position.
-	/// </summary>
-
 	void Update ()
 	{
-		if (mPlayer != null)
-		{
-			ownerID = mPlayer.id;
-			Vector3 delta = mTarget - mTrans.position;
-			mRb.AddForce(delta * 10f, ForceMode.Acceleration);
-		}
-		else ownerID = -1;
-
-		playerID = TNManager.player.id;
+		mTrans.position = Vector3.Lerp(mTrans.position, mTarget, Time.deltaTime * 20f);
 	}
 
 	/// <summary>
@@ -52,7 +34,7 @@ public class DraggedObject : TNBehaviour
 
 	void OnPress ()
 	{
-		if (mPlayer == null)
+		if (mOwner == null)
 		{
 			// Call the claim function directly in order to make it feel more responsive
 			ClaimObject(TNManager.playerID, mTrans.position);
@@ -63,16 +45,31 @@ public class DraggedObject : TNBehaviour
 	}
 
 	/// <summary>
-	/// When the object gets released, inform everyone that the player no longer has control.
+	/// When the mouse or touch gets released, inform everyone that the player no longer has control.
 	/// </summary>
 
 	void OnRelease ()
 	{
-		if (mPlayer == TNManager.player)
+		if (mOwner == TNManager.player)
 		{
 			ClaimObject(0, mTrans.position);
 			tno.Send(2, Target.OthersSaved, 0, mTrans.position);
 		}
+	}
+
+	/// <summary>
+	/// Remember the last player who claimed control of this object.
+	/// </summary>
+
+	[RFC(2)]
+	void ClaimObject (int playerID, Vector3 pos)
+	{
+		mOwner = TNManager.GetPlayer(playerID);
+		mTrans.position = pos;
+		mTarget = pos;
+
+		// Move the object to the Ignore Raycast layer while it's being dragged
+		gameObject.layer = LayerMask.NameToLayer((mOwner != null) ? "Ignore Raycast" : "Default");
 	}
 
 	/// <summary>
@@ -81,25 +78,11 @@ public class DraggedObject : TNBehaviour
 
 	void OnDrag (Vector2 delta)
 	{
-		if (mPlayer == TNManager.player)
+		if (mOwner == TNManager.player)
 		{
-			mTarget = TouchHandler.worldPos + Vector3.up;
+			mTarget = TouchHandler.worldPos;
 			tno.Send(3, Target.OthersSaved, mTarget);
 		}
-	}
-
-	/// <summary>
-	/// Remember the last player who claimed control of this object.
-	/// </summary>
-
-	[RFC(2)] void ClaimObject (int playerID, Vector3 pos)
-	{
-		mPlayer = TNManager.GetPlayer(playerID);
-		mTrans.position = pos;
-		mTarget = pos;
-
-		// Move the object to the Ignore Raycast layer while it's being dragged
-		gameObject.layer = LayerMask.NameToLayer( (mPlayer != null) ? "Ignore Raycast" : "Default");
 	}
 
 	/// <summary>
